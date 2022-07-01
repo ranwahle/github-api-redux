420eb2ec34f07d6503276a24682123d879f55b17
Ran <ran.wahle@gmail.com>
2022-07-01 13:25:49 +0300

    Add query-users

--- src/store/github-users-reducer.ts
+++ src/store/github-users-reducer.ts
@@ -1,10 +1,6 @@
 import { User } from "../model/user";
 
-const queryUsers = async(query: string) => {
-    const response = await fetch(`https://api.github.com/search/users?q=${query}`);
-    const data = await response.json();
-    return data.items;
-}
+
 
 export interface RootState {
     userState: UserState;
