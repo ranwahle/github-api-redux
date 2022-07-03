13202fe5cc7d4ea2fbb47d566a03b75233a039c8
Ran Wahle <ran.wahle@MBP-RAN.WAHLE>
2022-07-03 15:12:46 +0300

    Code cleanup

--- src/store/create-store.ts
+++ src/store/create-store.ts
@@ -1,13 +1,11 @@
-import { searchUsersMiddleware } from './search-users-middleware';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { legacy_createStore as createStore, applyMiddleware } from 'redux';
+import { conslogger, searchUsersMiddleware } from './search-users-middleware';
 import { rootReucer } from './root-reducer';
 import { configureStore } from '@reduxjs/toolkit';
 import { updateCurrentUserMiddleware } from './update-user-middleware';
 
 export const store = configureStore( {
     reducer: rootReucer,
-    middleware: [searchUsersMiddleware, updateCurrentUserMiddleware],
+    middleware: [searchUsersMiddleware, updateCurrentUserMiddleware, conslogger],
     devTools: true
 }
 // rootReucer,
--- src/store/search-users-middleware.ts
+++ src/store/search-users-middleware.ts
@@ -1,20 +1,20 @@
 import { CombinedState, Middleware } from "redux";
 import { queryUsers } from "../services/query-users";
-import { RootState } from "./github-users-reducer";
 
-export const searchUsersMiddleware: Middleware<{usersState: {}}, CombinedState<{usersState: {}}>> = store => next => action => {
-    if (action.type === 'SEARCH_USERS') {
-        queryUsers(action.payload).then(data => {
-            store.dispatch( { type: 'UPDATE_USERS', payload: data } );
-        });
+export const searchUsersMiddleware: Middleware<{ usersState: {} },
+    CombinedState<{ usersState: {} }>> = store => next => action => {
+        if (action.type === 'SEARCH_USERS') {
+            queryUsers(action.payload).then(data => {
+                store.dispatch({ type: 'UPDATE_USERS', payload: data });
+            });
+        }
+        return next(action);
     }
-    return next(action);
-}
 
 
 
-export const conslogger: Middleware<{}, RootState> =  store => next => action => {
+export const conslogger: Middleware<{}, CombinedState<{ usersState: {} }>> = store => next => action => {
     console.log(action);
     return next(action);
-        
+
 }
